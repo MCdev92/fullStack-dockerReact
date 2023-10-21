46da4366d1823ed4a537e1ff95d71ae5f428ebec
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-10-20 18:28:54 -0700

    backend ready to go

---
+++ backend/main.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from fastapi import FastAPI 
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Define your Recipe class
+class Recipe(BaseModel):
+    id: int
+    title: str
+    instructions: str
+        
+# Create database
+DB: List[Recipe] = [
+    Recipe(id=1, title="Panda Express Chicken Terriyaki", instructions="Add"),
+    Recipe(id=2, title="Spicy Thai Basil Chicken", instructions="Add"),
+    Recipe(id=3, title="Thai Red Curry with Chicken", instructions="Add"),
+    Recipe(id=4, title="Healthy Salmon Bowl", instructions="Add"),
+    Recipe(id=5, title="Instant Pot Chicken Tortilla Soup", instructions="Add"),
+    
+]
+
+# Change your path to api and return database
+@app.get("/api")
+def read_root():
+    return DB
